995a0de1a0e8ca48bf2dc59cb89f2d876c66d96a
chenqisheng <wsncqs@gmail.com>
2021-04-13 13:40:00 +0800

    feat(jm-api): 增加下单接口
    1. 增加直接下单预览/购物车下单预览的接口

---
+++ addons/@/apps/jm-shop-buss/src/main/java/cn/jimoos/form/order/CartForm.java
@@ -0,0 +1,19 @@
+package cn.jimoos.form.order;
+
+import cn.jimoos.utils.form.AbstractUserForm4L;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+/**
+ * 购物车 下单表单
+ *
+ * @author :keepcleargas
+ * @date :2021-04-13 11:29.
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class CartForm extends AbstractUserForm4L {
+    private List<CartItemForm> cartItems;
+}
--- starter/api-starter/src/main/java/cn/jimoos/rest/ProductApi.java
+++ starter/api-starter/src/main/java/cn/jimoos/rest/ProductApi.java
@@ -2,11 +2,20 @@ package cn.jimoos.rest;
 
 import cn.jimoos.common.exception.BussException;
 import cn.jimoos.entity.ProductEntity;
+import cn.jimoos.error.OrderError;
+import cn.jimoos.error.ProductError;
+import cn.jimoos.form.order.CartForm;
+import cn.jimoos.form.order.CartItemForm;
+import cn.jimoos.form.order.OrderItemForm;
 import cn.jimoos.form.product.ProductSearchForm;
 import cn.jimoos.model.UserProductCollection;
+import cn.jimoos.product.model.ProductItem;
+import cn.jimoos.product.provider.IProductProvider;
 import cn.jimoos.service.ProductCollectService;
 import cn.jimoos.service.ProductService;
 import cn.jimoos.vo.ProductVO;
+import cn.jimoos.vo.order.OrderPreVO;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -28,6 +37,8 @@ public class ProductApi {
     ProductService productService;
     @Resource
     ProductCollectService productCollectService;
+    @Resource
+    IProductProvider productProvider;
 
     /**
      * 获取商品详情信息
@@ -41,13 +52,75 @@ public class ProductApi {
                                 @PathVariable("productId") Long productId) throws BussException {
         ProductVO productVO = productService.getOne(productId);
 
-        if(productVO.getStatus() != ProductEntity.Status.LISTED.val()){
+        if (productVO.getStatus() != ProductEntity.Status.LISTED.val()) {
 
         }
         if (userId > 0) {
             productVO.setCollect(productCollectService.findByUserIdAndProductId(userId, productId) != null);
         }
         return productVO;
+    }
+
+    /**
+     * 立即下单
+     *
+     * @param userId    user Id
+     * @param productId product Id
+     * @param skuId     sku Id
+     * @param num       购买数量
+     * @return OrderPreVO 生成预览订单
+     */
+    @GetMapping(value = "/{productId}/skus/{skuId}/buy", produces = "application/json; charset=utf-8")
+    public OrderPreVO buy(@RequestParam("userId") Long userId,
+                          @PathVariable("productId") Long productId,
+                          @PathVariable("skuId") Long skuId,
+                          @RequestParam("num") Integer num) throws BussException {
+        ProductItem productItem = productProvider.byId(skuId);
+
+        if (productItem == null) {
+            throw new BussException(ProductError.PRODUCT_NOT_EXIST);
+        }
+
+        OrderPreVO result = new OrderPreVO();
+        result.setUserId(userId);
+        result.addOrderItem(new OrderItemForm(productItem, num));
+
+        //todo 优惠券可用 返回
+
+        return result;
+    }
+
+    /**
+     * 购物车下单
+     *
+     * @param cartForm 购物车表单
+     * @return OrderPreVO
+     * @throws BussException OrderError.CART_EMPTY
+     */
+    @PostMapping(value = "/cart/buy", produces = "application/json; charset=utf-8")
+    public OrderPreVO buy(@RequestBody CartForm cartForm) throws BussException {
+        if (CollectionUtils.isEmpty(cartForm.getCartItems())) {
+            throw new BussException(OrderError.CART_EMPTY);
+        }
+
+        OrderPreVO result = new OrderPreVO();
+
+        result.setUserId(cartForm.getUserId());
+
+
+        List<ProductItem> productItems = productProvider.byIds(cartForm.getCartItems().stream().map(CartItemForm::getSkuId).collect(Collectors.toList()));
+        Map<Long, Integer> idToNumberFormMap = cartForm.getCartItems().stream().collect(Collectors.toMap(CartItemForm::getSkuId, CartItemForm::getNumber));
+
+        if (CollectionUtils.isEmpty(productItems)) {
+            throw new BussException(ProductError.PRODUCT_NOT_EXIST);
+        }
+
+        for (ProductItem productItem : productItems) {
+            result.addOrderItem(new OrderItemForm(productItem, idToNumberFormMap.get(productItem.getProductId())));
+        }
+        //todo 优惠券可用 返回
+
+        return result;
     }
 
     /**

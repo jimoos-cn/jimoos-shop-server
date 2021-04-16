3b97c04e810708a0dd382b25823ecd7eac3b57ac
chenqisheng <wsncqs@gmail.com>
2021-04-16 22:45:09 +0800

    feat(jm-payment): 引入了 微信第三方包

---
+++ starter/api-starter/src/main/java/cn/jimoos/rest/PayApi.java
@@ -0,0 +1,15 @@
+package cn.jimoos.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author :keepcleargas
+ * @date :2021-04-16 22:39.
+ */
+@RestController
+@RequestMapping("/v1/payments")
+@Slf4j
+public class PayApi {
+}

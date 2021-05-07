b00bff504576c9900dde5127e218e01098b439a6
chenqisheng <wsncqs@gmail.com>
2021-05-07 11:01:50 +0800

    feat(portal-starter): 增加管理员登录 AuthApi 接口

---
+++ starter/portal-starter/src/main/java/cn/jimoos/rest/be/BeAuthApi.java
@@ -0,0 +1,40 @@
+package cn.jimoos.rest.be;
+
+import cn.jimoos.common.exception.BussException;
+import cn.jimoos.form.AdminLoginForm;
+import cn.jimoos.service.AdminService;
+import cn.jimoos.vo.AdminVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+/**
+ * 授权登录
+ *
+ * @author :keepcleargas
+ * @date :2021-05-07 10:39.
+ */
+@RestController
+@RequestMapping("/bAdmin/v1")
+@Slf4j
+public class BeAuthApi {
+    @Resource
+    AdminService adminService;
+
+    /**
+     * 更新密码
+     *
+     * @param adminLoginForm 登录表单
+     * @throws BussException AdminError.ADMIN_NOT_EXIST
+     */
+    @PostMapping(value = "/login", produces = "application/json; charset=utf-8")
+    public AdminVO updatePwd(HttpServletRequest request, @ModelAttribute @Valid AdminLoginForm adminLoginForm) throws BussException {
+        return adminService.login(request, adminLoginForm);
+    }
+}

47a32ccf4830e73acc6745c071388c8c917121ac
qisheng.chen <qisheng.chen@xiaohuodui.cn>
2021-09-29 09:49:19 +0800

    feat(migration): 迁移完数据后，自动关闭应用

--- migration/src/main/java/cn/jimoos/MigrationApp.java
+++ migration/src/main/java/cn/jimoos/MigrationApp.java
@@ -2,6 +2,7 @@ package cn.jimoos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Migration APP
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"cn.jimoos"})
 public class MigrationApp {
     public static void main(String[] args) {
-        SpringApplication.run(MigrationApp.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(MigrationApp.class, args);
+        ctx.close();
     }
 }

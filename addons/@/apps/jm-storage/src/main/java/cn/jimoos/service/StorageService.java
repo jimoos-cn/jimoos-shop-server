00660b14e93e43a884a8e6ea1ac025ab74ee223e
yanyuxi <yuxi.yan@xiaohuodui.cn>
2021-10-08 10:20:14 +0800

    fix(local storage): 本地存储修改

--- addons/@/apps/jm-storage/src/main/java/cn/jimoos/service/StorageService.java
+++ addons/@/apps/jm-storage/src/main/java/cn/jimoos/service/StorageService.java
@@ -1,30 +1,30 @@
 package cn.jimoos.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 
 @Service
 public class StorageService {
-    @Value("${local.storage.root-path}")
-    private String ROOT_PATH;
+    @Resource
+    private String rootPath;
 
     public String upload(MultipartFile file, int type, HttpServletRequest request) throws IOException {
         String oldName = file.getOriginalFilename();
         Assert.notNull(oldName, "文件名为空");
         String folderPath = type == 0 ? "image/" : "media/";
-        File folder = new File(ROOT_PATH + folderPath);
+        File folder = new File(rootPath + folderPath);
         if (!folder.isDirectory()) {
             folder.mkdir();
         }
         String newName = System.currentTimeMillis() +
                 oldName.substring(oldName.lastIndexOf("."));
-        file.transferTo(new File(ROOT_PATH + folderPath + newName));
+        file.transferTo(new File(rootPath + folderPath + newName));
         return request.getScheme() + "://" + request.getServerName()
                 + ":" + request.getServerPort()  + "/storage/" + folderPath + newName;
     }
@@ -33,7 +33,7 @@ public class StorageService {
         String[] split = url.split("/");
         String folder = split[split.length - 2];
         String fileName = split[split.length - 1];
-        String filePath = ROOT_PATH + "/" + folder + "/" + fileName;
+        String filePath = rootPath + "/" + folder + "/" + fileName;
         File file = new File(filePath);
         if (file.exists()) {
             file.delete();
--- starter/portal-starter/src/main/java/cn/jimoos/config/WebConfiguration.java
+++ starter/portal-starter/src/main/java/cn/jimoos/config/WebConfiguration.java
@@ -3,8 +3,6 @@ package cn.jimoos.config;
 
 import cn.jimoos.filter.CorsFilter;
 import cn.jimoos.filter.TokenInterceptor;
-import com.baomidou.mybatisplus.extension.api.R;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +11,15 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.Resource;
+
 /**
  * @author qisheng.chen
  */
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
-    @Value("${local.storage.root-path}")
-    private String ROOT_PATH;
+    @Resource
+    private String rootPath;
 
     @Bean
     @ConditionalOnMissingBean
@@ -46,7 +46,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/storage/**")
-                .addResourceLocations("file:" + ROOT_PATH);
+                .addResourceLocations("file:" + rootPath);
         WebMvcConfigurer.super.addResourceHandlers(registry);
     }
 }

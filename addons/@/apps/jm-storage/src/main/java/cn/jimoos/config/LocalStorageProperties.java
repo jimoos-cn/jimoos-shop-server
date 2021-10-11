90f7b3ec64d6b38b10d83e68a6299e37b0ed3487
yanyuxi <yuxi.yan@xiaohuodui.cn>
2021-10-11 20:09:44 +0800

    fix(storage): 上传可配置

--- addons/@/apps/jm-storage/src/main/java/cn/jimoos/config/LocalStorageProperties.java
+++ addons/@/apps/jm-storage/src/main/java/cn/jimoos/config/LocalStorageProperties.java
@@ -1,8 +1,12 @@
 package cn.jimoos.config;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LocalStorageProperties {
     public final static String KEY = "local.storage";
     private String rootPath;
--- addons/@/apps/jm-storage/src/main/java/cn/jimoos/service/StorageService.java
+++ addons/@/apps/jm-storage/src/main/java/cn/jimoos/service/StorageService.java
@@ -1,11 +1,15 @@
 package cn.jimoos.service;
 
+import cn.jimoos.config.LocalStorageProperties;
+import cn.jimoos.config.StorageProperties;
+import cn.jimoos.form.BaseSettingsCreateForm;
+import cn.jimoos.service.impl.BaseSettingsService;
+import cn.jimoos.utils.mapper.JsonMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 
@@ -17,7 +21,10 @@ public class StorageService {
     @Resource
     private String host;
 
-    public String upload(MultipartFile file, int type, HttpServletRequest request) throws IOException {
+    @Resource
+    BaseSettingsService baseSettingsService;
+
+    public String upload(MultipartFile file, int type) throws IOException {
         String oldName = file.getOriginalFilename();
         Assert.notNull(oldName, "文件名为空");
         String folderPath = type == 0 ? "image/" : "media/";
@@ -40,5 +47,40 @@ public class StorageService {
         if (file.exists()) {
             file.delete();
         }
+    }
+
+    public StorageProperties checkStorage() {
+        StorageProperties storageProperties =
+                baseSettingsService.getObjectByKeyword(StorageProperties.KEY, StorageProperties.class);
+        return storageProperties == null ? defaultStorage() : storageProperties;
+    }
+
+    public StorageProperties changeStorage() {
+        StorageProperties storageProperties =
+                baseSettingsService.getObjectByKeyword(StorageProperties.KEY, StorageProperties.class);
+        Boolean temp = storageProperties.getLocalStorage();
+        storageProperties.setLocalStorage(!temp);
+        storageProperties.setHuaweiObs(temp);
+        String content = JsonMapper.INSTANCE.toJson(storageProperties);
+        baseSettingsService.saveContentByKeyword(content, StorageProperties.KEY);
+        return storageProperties;
+    }
+
+    private StorageProperties defaultStorage() {
+        StorageProperties storageProperties = new StorageProperties(true, false);
+        BaseSettingsCreateForm form = new BaseSettingsCreateForm();
+        form.setContent(JsonMapper.INSTANCE.toJson(storageProperties));
+        form.setKeyword(StorageProperties.KEY);
+        baseSettingsService.save(form);
+
+        LocalStorageProperties localStorageProperties =
+                baseSettingsService.getObjectByKeyword(LocalStorageProperties.KEY, LocalStorageProperties.class);
+        if (localStorageProperties == null) {
+            localStorageProperties = new LocalStorageProperties(rootPath, host);
+            form.setContent(JsonMapper.INSTANCE.toJson(localStorageProperties));
+            form.setKeyword(LocalStorageProperties.KEY);
+            baseSettingsService.save(form);
+        }
+        return storageProperties;
     }
 }
